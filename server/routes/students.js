b3858ec468588fe408b1cc4b3c35e2abd05e7b05
Shwetang <shwetangsingh5@gmail.com>
2021-09-12 11:51:02 +0530

    created model and get req

--- server/routes/students.js
+++ server/routes/students.js
@@ -1,9 +1,52 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello get-student...');
+// student schema
+const studentSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3
+    },
+    registration_number: {
+        type: Number,
+        required: true,
+    },
+    grade: {
+        type: String,
+        required: true,
+    },
+    isStudent: {
+        type: Boolean,
+        required: function () {
+            if (parseInt(this.grade) >= 1 && parseInt(this.grade) <= 12)
+                return true;
+            else
+                return false;
+        }
+    },
+    section: {
+        type: String,
+        default: 'A',
+    },
+    subjects: [String]
+});
+
+// creating a model of studentSchema
+const Student = mongoose.model('Student', studentSchema);
+
+router.get('/', async (req, res) => {
+    try {
+        const allStudentList = await Student.find();
+
+        // if allStudentList != 0
+        res.status(200).json(allStudentList);
+    } catch (error) {
+        // if allStudentList == 0 or some other errors
+        res.status(404).json({ message: error.message });
+    }
 });
 
 // exporting the student routes
